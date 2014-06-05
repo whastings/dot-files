7da73f113014255d6d22ced173252d6bfcb32875
Will Hastings <will@objectdotcreate.net>
2014-06-05 11:20:28 -0700

    Enable history search with arrow keys in zsh.

--- zsh/plugins/whastings/whastings.plugin.zsh
+++ zsh/plugins/whastings/whastings.plugin.zsh
@@ -15,3 +15,11 @@ bindkey '^g' git_diff_cached
 git_add() { \print; zle accept-line; ga; zle accept-line }
 zle -N git_add
 bindkey '^p' git_add
+
+# Enable history search via arrow keys.
+autoload -U up-line-or-beginning-search
+autoload -U down-line-or-beginning-search
+zle -N up-line-or-beginning-search
+zle -N down-line-or-beginning-search
+bindkey "^[[A" up-line-or-beginning-search # Up
+bindkey "^[[B" down-line-or-beginning-search # Down
