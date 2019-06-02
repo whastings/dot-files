94800e20096aa61a5b7868a6e89dd2377d4f393d
Will Hastings <will@objectdotcreate.net>
2019-06-01 19:53:20 -0700

    Add more git aliases

--- aliases.sh
+++ aliases.sh
@@ -1,7 +1,9 @@
 # Git:
 alias gv="git pull"
-alias g^="git push"
+alias g^="git push origin HEAD"
+alias gb="git branch"
 alias gc="git commit"
+alias gco="git checkout"
 alias gds="git diff --staged"
-alias gl="git log"
+alias gl="git log --color --graph --pretty=format:'%Cred%h%Creset - %s %Cgreen(%cr)%C(bold blue) <%an>%Creset%C(yellow)%d%Creset' --abbrev-commit"
 alias gst="git status"
